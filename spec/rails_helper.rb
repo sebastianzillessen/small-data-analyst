feb4f49face2d79816ef22d6e53dcaa20fb9faaf
Sebastian Zillessen <sebastian@zillessen.info>
2016-06-25 22:08:58 +0100

    fixed specs, added delayed job

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,6 +65,7 @@ RSpec.configure do |config|
 
 
   config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, :type => :view
   config.extend ControllerMacros, type: :controller
   config.include DeviseRequestSpecHelpers, type: :request
 
