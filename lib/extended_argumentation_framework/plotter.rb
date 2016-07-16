5c5c2b76cec701bae182e79640a5f8545d6b1133
Sebastian Zillessen <sebastian@zillessen.info>
2016-07-16 18:11:06 +0100

    fixed some problems with EAF-UI

--- lib/extended_argumentation_framework/plotter.rb
+++ lib/extended_argumentation_framework/plotter.rb
@@ -23,7 +23,8 @@ module ExtendedArgumentationFramework
       @nodes_in =[]
       @nodes_out = []
       @nodes_undec = []
-      @framework.arguments.each do |a|
+      @framework.arguments.reject { |a| @arguments_hold.include?(a) }.each do |a|
+
         res = @solver.acceptable_arguments(@arguments_hold, a)
         if (res.nil?)
           @nodes_undec << a.int_name
@@ -36,6 +37,7 @@ module ExtendedArgumentationFramework
 
 
       "{ node[fillcolor=\"green\" style=\"filled\"] #{@nodes_in.join(" ")} }
+      { node[fillcolor=\"green\" style=\"filled\" shape=\"box\"] #{@arguments_hold.map(&:int_name).join(" ")} }
       { node[fillcolor=\"red\" style=\"filled\"] #{@nodes_out.join(" ")} }
       { node[fillcolor=\"grey\" style=\"filled\"] #{@nodes_undec.join(" ")} }
       "
@@ -43,13 +45,14 @@ module ExtendedArgumentationFramework
 
     def dot_tempnodes
       @framework.attacks_on_attacks.map do |a|
-        "#{a.target.source.int_name}__#{a.target.target.int_name}"
-      end
+        edge_name="#{a.target.source.int_name}__#{a.target.target.int_name}"
+        "#{edge_name} [group=#{edge_name} width=0 shape=point]"
+      end.flatten.uniq
     end
 
     def dot_edges_without_arrow
       @framework.attacks_on_attacks.map do |a|
-        attr = if (@nodes_in.include?(a.source.int_name))
+        attr = if ((@nodes_in+@arguments_hold.map(&:int_name)).include?(a.source.int_name))
                  "[style=dashed]"
                end
         "#{a.target.source.int_name} -> #{a.target.source.int_name}__#{a.target.target.int_name} #{attr}"
@@ -58,7 +61,7 @@ module ExtendedArgumentationFramework
 
     def dot_edges
       res = @framework.attacks_on_attacks.map { |a|
-        attr = if (@nodes_in.include?(a.source.int_name))
+        attr = if ((@nodes_in+@arguments_hold.map(&:int_name)).include?(a.source.int_name))
                  "[style=dashed]"
                end
         "#{a.target.source.int_name}__#{a.target.target.int_name} -> #{a.target.target.int_name} #{attr}"
@@ -71,11 +74,11 @@ module ExtendedArgumentationFramework
 
     def to_dot
       "digraph G {
-        {node[width=0 shape=point] #{dot_tempnodes.join("; ")}}
-        #{dot_nodes}
-        {edge[arrowhead=none] #{dot_edges_without_arrow.join("; ")}}
-        #{dot_edges.join("; ")}
-      }"
+        #{dot_tempnodes.join("; ")};
+        #{dot_nodes};
+        {edge[arrowhead=none] #{dot_edges_without_arrow.join("; ")}};
+        #{dot_edges.join("; ")};
+      }".tap { |x| puts x }
     end
 
 
