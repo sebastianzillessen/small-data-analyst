5945ab5e43625869c6d63967ce7fce9d4097895a
Sebastian Zillessen <sebastian@zillessen.info>
2016-07-23 14:01:47 +0100

    fixed ui specs

--- spec/views/research_questions/show.html.haml_spec.rb
+++ spec/views/research_questions/show.html.haml_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "research_questions/show", type: :view do
   subject { FactoryGirl.create(:research_question) }
+  let(:user) { create(:user) }
+  before(:each) { allow(controller).to receive(:current_user).and_return(user) }
+
   before(:each) do
     @research_question = assign(:research_question, subject)
   end
